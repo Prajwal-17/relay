c48d6e2c34dff38ea1d5feed00be4f530e294fe8
prajwal <prajwalkreddy17@gmail.com>
2025-08-22 23:42:04 +0530

    fix(schema): fix createdAt & updatedAt fields causing null errors

--- src/main/db/schema.ts
+++ src/main/db/schema.ts
@@ -9,8 +9,8 @@ export const users = sqliteTable("users", {
   name: text("name").notNull(),
   role: text("role").notNull(),
   password: text("password").notNull(),
-  createdAt: text("created_at").default(sql`(datetime('now'))`),
-  updatedAt: text("updated_at").default(sql`(datetime('now'))`)
+  createdAt: text("created_at").default(sql`(datetime('now'))`).notNull(),
+  updatedAt: text("updated_at").default(sql`(datetime('now'))`).notNull()
 });
 
 export const customers = sqliteTable("customers", {
@@ -18,10 +18,10 @@ export const customers = sqliteTable("customers", {
     .primaryKey()
     .$defaultFn(() => uuidv4()),
   name: text("name").notNull(),
-  contact: text("contact").notNull(),
+  contact: text("contact"),
   customerType: text("customer_type"), // "cash" | "account" | "hotel"
-  createdAt: text("created_at").default(sql`(datetime('now'))`),
-  updatedAt: text("updated_at").default(sql`(datetime('now'))`)
+  createdAt: text("created_at").default(sql`(datetime('now'))`).notNull(),
+  updatedAt: text("updated_at").default(sql`(datetime('now'))`).notNull()
 });
 
 export const products = sqliteTable("products", {
@@ -35,11 +35,11 @@ export const products = sqliteTable("products", {
   price: integer("price").notNull(),
   totalQuantitySold: integer("total_quantity_sold").default(0),
   isDisabled: integer("is_disabled", { mode: "boolean" }).notNull().default(false),
-  disabledAt: text("disabled_at").default(sql`(datetime('now'))`),
+  disabledAt: text("disabled_at"),
   isDeleted: integer("is_deleted", { mode: "boolean" }).notNull().default(false),
-  deletedAt: text("deleted_at").default(sql`(datetime('now'))`),
-  createdAt: text("created_at").default(sql`(datetime('now'))`),
-  updatedAt: text("updated_at").default(sql`(datetime('now'))`)
+  deletedAt: text("deleted_at"),
+  createdAt: text("created_at").default(sql`(datetime('now'))`).notNull(),
+  updatedAt: text("updated_at").default(sql`(datetime('now'))`).notNull()
 });
 
 export const productHistory = sqliteTable("product_history", {
@@ -56,8 +56,8 @@ export const productHistory = sqliteTable("product_history", {
   newPrice: integer("new_price"),
   oldMrp: integer("old_mrp"),
   newMrp: integer("new_mrp"),
-  createdAt: text("created_at").default(sql`(datetime('now'))`),
-  updatedAt: text("updated_at").default(sql`(datetime('now'))`)
+  createdAt: text("created_at").default(sql`(datetime('now'))`).notNull(),
+  updatedAt: text("updated_at").default(sql`(datetime('now'))`).notNull()
 });
 
 export const sales = sqliteTable("sales", {
@@ -66,13 +66,13 @@ export const sales = sqliteTable("sales", {
     .$defaultFn(() => uuidv4()),
   invoiceNo: integer("invoice_no").notNull(),
   customerId: text("customer_id").references(() => customers.id),
-  customerName: text("customer_name").notNull(),
+  customerName: text("customer_name").notNull(), // "name = DEFAULT"
   customerContact: text("customer_contact"),
   grandTotal: integer("grand_total", { mode: "number" }),
   totalQuantity: real("total_quantity"),
   isPaid: integer("is_paid", { mode: "boolean" }).notNull().default(true),
-  createdAt: text("created_at").default(sql`(datetime('now'))`),
-  updatedAt: text("updated_at").default(sql`(datetime('now'))`)
+  createdAt: text("created_at").default(sql`(datetime('now'))`).notNull(),
+  updatedAt: text("updated_at").default(sql`(datetime('now'))`).notNull()
 });
 
 export const saleItems = sqliteTable("sale_items", {
@@ -90,8 +90,8 @@ export const saleItems = sqliteTable("sale_items", {
   unit: text("unit"),
   quantity: integer("quantity").notNull(),
   totalPrice: integer("total_price").notNull(),
-  createdAt: text("created_at").default(sql`(datetime('now'))`),
-  updatedAt: text("updated_at").default(sql`(datetime('now'))`)
+  createdAt: text("created_at").default(sql`(datetime('now'))`).notNull(),
+  updatedAt: text("updated_at").default(sql`(datetime('now'))`).notNull()
 });
 
 export const estimates = sqliteTable("estimates", {
@@ -105,8 +105,8 @@ export const estimates = sqliteTable("estimates", {
   grandTotal: integer("grand_total", { mode: "number" }),
   totalQuantity: real("total_quantity"),
   isPaid: integer("is_paid", { mode: "boolean" }).default(true),
-  createdAt: text("created_at").default(sql`(datetime('now'))`),
-  updatedAt: text("updated_at").default(sql`(datetime('now'))`)
+  createdAt: text("created_at").default(sql`(datetime('now'))`).notNull(),
+  updatedAt: text("updated_at").default(sql`(datetime('now'))`).notNull()
 });
 
 export const estimateItems = sqliteTable("estimate_items", {
@@ -124,6 +124,6 @@ export const estimateItems = sqliteTable("estimate_items", {
   unit: text("unit"),
   quantity: integer("quantity").notNull(),
   totalPrice: integer("total_price").notNull(),
-  createdAt: text("created_at").default(sql`(datetime('now'))`),
-  updatedAt: text("updated_at").default(sql`(datetime('now'))`)
+  createdAt: text("created_at").default(sql`(datetime('now'))`).notNull(),
+  updatedAt: text("updated_at").default(sql`(datetime('now'))`).notNull()
 });
